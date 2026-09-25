c6fb5ed3e91eec1da75359181be626613a2dc28e
GabyY <gab.basque11@gmail.com>
2026-09-25 17:01:05 +0200

    Use single-level Nelya API hostname

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window.NELYA_CONFIG = {
   VERSION: "0.1.1",
 
-  API_URL: "https://api.nelya.horibli.com",
+  API_URL: "https://nelya-api.horibli.com",
 
   CHAT_ENDPOINT: "/chat",
   FEEDBACK_ENDPOINT: "/feedback"
